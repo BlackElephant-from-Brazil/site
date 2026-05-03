8af20179390931e06f4b3e7eba2ee73e0ec440fc
Guilherme Kodenvis <guilherme@blackelephant.com.br>
2026-05-03 17:02:29 -0300

    feat(kanban): fetch clients and currentUserId in page

--- src/app/[locale]/(member)/dashboard/admin/kanban/page.tsx
+++ src/app/[locale]/(member)/dashboard/admin/kanban/page.tsx
@@ -1,7 +1,8 @@
 import { setRequestLocale } from 'next-intl/server'
 import { getKanbanBoard } from '@/lib/actions/kanban-cards'
 import { getProjects } from '@/lib/actions/projects'
-import { getAdminUsers } from '@/lib/actions/users'
+import { getAdminUsers, getCurrentUserPublicId } from '@/lib/actions/users'
+import { getClients } from '@/lib/actions/clients'
 import { KanbanBoard } from '@/components/admin/kanban/KanbanBoard'
 
 export const dynamic = 'force-dynamic'
@@ -11,10 +12,20 @@ type Params = Promise<{ locale: string }>
 export default async function KanbanPage({ params }: { params: Params }) {
   const { locale } = await params
   setRequestLocale(locale)
-  const [board, projectsWithRefs, adminUsers] = await Promise.all([
+  const [board, projectsWithRefs, adminUsers, clients, currentUserId] = await Promise.all([
     getKanbanBoard(),
     getProjects(),
     getAdminUsers(),
+    getClients(),
+    getCurrentUserPublicId(),
   ])
-  return <KanbanBoard initialBoard={board} projects={projectsWithRefs} adminUsers={adminUsers} />
+  return (
+    <KanbanBoard
+      initialBoard={board}
+      projects={projectsWithRefs}
+      adminUsers={adminUsers}
+      clients={clients}
+      currentUserId={currentUserId}
+    />
+  )
 }
